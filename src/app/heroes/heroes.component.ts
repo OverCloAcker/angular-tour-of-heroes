4217c4bb8e7e02d238d5cb209985a89b3a1285d1
Sergey Prilipko <sprilipko@gmail.com>
2022-12-07 15:50:51 +0300

    MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -19,10 +20,16 @@ export class HeroesComponent {
   selectedHero?: Hero;
 
   // constructor() { }
-  constructor(private heroService: HeroService) { }
+  // constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // getHeroes(): void {
